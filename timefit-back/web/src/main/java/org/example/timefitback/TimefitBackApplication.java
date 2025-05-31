215bf08363428ac07760958edf3adb99147c7347
Sechang Jang <71550827+sehan528@users.noreply.github.com>
2025-05-31 13:10:48 +0900

    feat: 멀티 모듈 구조 설계 적용

--- timefit-back/web/src/main/java/org/example/timefitback/TimefitBackApplication.java
+++ timefit-back/web/src/main/java/org/example/timefitback/TimefitBackApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TimefitBackApplication {
-
     public static void main(String[] args) {
+
         SpringApplication.run(TimefitBackApplication.class, args);
     }
-
 }
+
--- timefit-back/web/src/test/java/org/example/timefitback/TimefitBackApplicationTests.java
+++ timefit-back/web/src/test/java/org/example/timefitback/TimefitBackApplicationTests.java
@@ -2,8 +2,11 @@ package org.example.timefitback;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TimefitBackApplicationTests {
 
     @Test
