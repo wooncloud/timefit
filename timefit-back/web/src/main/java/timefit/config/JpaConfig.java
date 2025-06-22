465e675407fa45ce67dbdef2f0b2c04045128a93
Sechang Jang <71550827+sehan528@users.noreply.github.com>
2025-06-22 15:03:57 +0900

    refactor: Package 경로 간소화로 인한 경로 수정
    - 도메인 표기를 명확하게 하기 위해 기존의 'org.example' 표기를 삭제.
    - 변경으로 인해 기존 import 경로 업데이트.

--- timefit-back/web/src/main/java/timefit/config/JpaConfig.java
+++ timefit-back/web/src/main/java/timefit/config/JpaConfig.java
@@ -1,4 +1,4 @@
-package org.example.timefitback.common.config;
+package timefit.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
