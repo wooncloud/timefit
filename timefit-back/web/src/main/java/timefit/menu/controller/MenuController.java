578882dc582a1acc652cfd5f1ec2492e4fdf7d00
Sechang Jang <71550827+sehan528@users.noreply.github.com>
2025-11-03 15:26:12 +0900

    chore(menu): 잘못 기입된 API 엔드포인트 수정

--- timefit-back/web/src/main/java/timefit/menu/controller/MenuController.java
+++ timefit-back/web/src/main/java/timefit/menu/controller/MenuController.java
@@ -23,7 +23,7 @@ import java.util.UUID;
  */
 @Slf4j
 @RestController
-@RequestMapping("/api/businesses/{businessId}/menus")
+@RequestMapping("/api/businesses/{businessId}/menu")
 @RequiredArgsConstructor
 public class MenuController {
 
