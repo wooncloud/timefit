06ff3280804acf289dc93d81d428e2080955ab7c
Sechang Jang <71550827+sehan528@users.noreply.github.com>
2025-11-14 22:26:15 +0900

    refactor(menu): 불필요한 DTO 삭제, 응답 DTO record 클래스로 변경

--- timefit-back/web/src/main/java/timefit/menu/dto/MenuListResponse.java
+++ timefit-back/web/src/main/java/timefit/menu/dto/MenuListResponse.java
@@ -1,42 +1,23 @@
 package timefit.menu.dto;
 
-import lombok.Getter;
 import timefit.menu.entity.Menu;
-
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
-@Getter
-public class MenuListResponse {
-
-    private final List<MenuSummary> menus;
-    private final Integer totalCount;
-
-    private MenuListResponse(List<MenuSummary> menus, Integer totalCount) {
-        this.menus = menus;
-        this.totalCount = totalCount;
-    }
-
-    // 정적 팩토리 메서드
+public record MenuListResponse(
+        List<MenuResponse> menus,
+        Integer totalCount
+) {
+    /**
+     * Entity List → DTO 변환 (정적 팩토리)
+     *
+     * @param menuEntities Menu 엔티티 리스트
+     * @return MenuListResponse DTO
+     */
     public static MenuListResponse of(List<Menu> menuEntities) {
-        List<MenuSummary> menus = menuEntities.stream()
-                .map(MenuSummary::of)
-                .collect(Collectors.toList());
+        List<MenuResponse> menus = menuEntities.stream()
+                .map(MenuResponse::from)
+                .toList();
+
         return new MenuListResponse(menus, menus.size());
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        if (this == other) return true;
-        if (other == null || getClass() != other.getClass()) return false;
-        MenuListResponse that = (MenuListResponse) other;
-        return Objects.equals(menus, that.menus) &&
-                Objects.equals(totalCount, that.totalCount);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(menus, totalCount);
     }
 }
