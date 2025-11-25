521b151c9835f1052a7549db28603154e496324b
Sechang Jang <71550827+sehan528@users.noreply.github.com>
2025-11-26 00:38:39 +0900

    fix(menu): toggle, menu 영구삭제 -> 논리삭제 원복
    related to: #49

--- timefit-back/web/src/main/java/timefit/menu/service/MenuService.java
+++ timefit-back/web/src/main/java/timefit/menu/service/MenuService.java
@@ -62,6 +62,16 @@ public class MenuService {
         return menuCommandService.updateMenu(businessId, menuId, request, currentUserId);
     }
 
+    // 메뉴 활성/비활성 토글
+    @Transactional
+    public MenuResponseDto.Menu toggleMenuActive(
+            UUID businessId,
+            UUID menuId,
+            UUID currentUserId) {
+
+        return menuCommandService.toggleMenuActive(businessId, menuId, currentUserId);
+    }
+
     // 메뉴 삭제 (비활성화)
     @Transactional
     public void deleteMenu(UUID businessId, UUID menuId, UUID currentUserId) {
