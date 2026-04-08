2654d2216e89936630cbd2f96fb74b806804b3b2
Sechang Jang <71550827+sehan528@users.noreply.github.com>
2026-04-08 17:54:09 +0900

    feat(calendar): '시간 단위' 타입 변수 분리
    rel: #88

---
+++ timefit-front/src/types/business/calendar.ts
@@ -0,0 +1,8 @@
+/**
+ * 캘린더 뷰 타입
+ * month: 월 뷰 (커스텀)
+ * timeGridWeek: 주 뷰 (FullCalendar)
+ * timeGridDay: 일 뷰 (FullCalendar)
+ * year: 연 뷰 (커스텀)
+ */
+export type ViewType = 'month' | 'timeGridWeek' | 'timeGridDay' | 'year';
